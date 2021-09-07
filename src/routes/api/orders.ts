4ffcc4d904721a72be03196c29e6cafa838c273a
Troy Hayman <THayman@innout.com>
2021-09-07 10:48:16 -0700

    deleted unused folder

--- src/routes/api/orders.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Order, OrderStatusStore, OrderStore } from '../../models/order';
-const orders = express.Router();
-
-orders.get('/', (req, res) => {
-    res.send('Orders route');
-});
-
-export default orders;
--- src/routes/api/products.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Product, ProductStore } from '../../models/product';
-const products = express.Router();
-
-products.get('/', (req, res) => {
-    res.send('Products route');    
-});
-
-export default products;
